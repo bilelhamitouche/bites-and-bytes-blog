3c0181c045287d71807204211c61814befa5a270
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-01-29 00:54:52 +0100

    added the auth middleware for posts, users and comments routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,23 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
-import auth from "./auth";
+import auth, { authMiddleware } from "./auth";
 
 // App initialization
 const app = new Hono().basePath("/api");
+
+const routes = ["/posts/*", "/comments/*", "/users/*"];
 
 // Middleware
 app.use(cors({ origin: ["", ""] }));
 
 // Routers
 app.route("/auth", auth);
+routes.forEach((route) => {
+  app.use(route, authMiddleware);
+});
+app.get("/posts", (c) => {
+  return c.html("Hello world from protected route");
+});
 
 app.onError(async (err, c) => {
   return c.json({ message: "Error occured", err: err });
