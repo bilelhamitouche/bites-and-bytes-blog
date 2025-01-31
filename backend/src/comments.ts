a7ff6f4a79976453cd73d80ead4587b6f9384e98
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-01-31 01:33:01 +0100

    added crud operations in crud (routes and handlers)

--- backend/src/comments.ts
+++ backend/src/comments.ts
@@ -1,6 +1,39 @@
 import { Hono } from "hono";
 import { Env } from "./types";
+import {
+  createComment,
+  deleteComment,
+  getComments,
+  updateComment,
+} from "../db/comments";
+import { getUserInfo } from "./utils";
 
 const app = new Hono<Env>();
+
+app.get("/", async (c) => {
+  const comments = await getComments();
+  return c.json(comments);
+});
+
+app.post("/", async (c) => {
+  const { content, postId } = await c.req.json();
+  const { id } = await getUserInfo(c);
+  await createComment(content, id as number, postId);
+  return c.json({ message: "Comment created successfully" });
+});
+
+app.put("/:id", async (c) => {
+  const id = c.req.param("id");
+  const { content } = await c.req.json();
+  const updatedComment = await updateComment(parseInt(id), content);
+  console.log(updatedComment);
+  return c.json({ message: "Comment updated successfully" });
+});
+
+app.delete("/:id", async (c) => {
+  const id = c.req.param("id");
+  await deleteComment(parseInt(id));
+  return c.json({ message: "Comment deleted successfully" });
+});
 
 export default app;
