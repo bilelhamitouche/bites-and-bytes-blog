3369c830ecf1ff1de8f21855b3e06ab762e2dde1
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-01-31 01:33:30 +0100

    added crud operations in users(routes and handlers)

--- backend/src/users.ts
+++ backend/src/users.ts
@@ -1,21 +1,22 @@
 import { Hono } from "hono";
 import { createUser, deleteUser, getUsers } from "../db/users";
 import { Env } from "./types";
+import { authorizeAdminMiddleware } from "./auth";
 
 const app = new Hono<Env>();
 
-app.get("/", async (c) => {
+app.get("/", authorizeAdminMiddleware, async (c) => {
   const users = await getUsers();
   return c.json(users, 200);
 });
 
-app.post("/", async (c) => {
+app.post("/", authorizeAdminMiddleware, async (c) => {
   const { username, email, password } = await c.req.json();
   await createUser(username, email, password);
   return c.json({ message: "User created successfully" }, 201);
 });
 
-app.delete("/:id", async (c) => {
+app.delete("/:id", authorizeAdminMiddleware, async (c) => {
   const { id } = await c.req.json();
   await deleteUser(id);
   return c.json({ message: "User deleted successfully" }, 202);
