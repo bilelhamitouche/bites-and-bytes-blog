458ee227dda6f6c8cd6cf4334be593f56db06092
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-01-31 01:33:18 +0100

    added crud operations in post(routes and handlers)

--- backend/src/posts.ts
+++ backend/src/posts.ts
@@ -1,6 +1,7 @@
 import { Hono } from "hono";
-import { createPost, getPosts } from "../db/posts";
+import { createPost, deletePost, getPosts, updatePost } from "../db/posts";
 import { Env } from "./types";
+import { authorizeAdminMiddleware } from "./auth";
 
 const app = new Hono<Env>();
 
@@ -9,13 +10,23 @@ app.get("/", async (c) => {
   return c.json(posts, 200);
 });
 
-app.post("/", async (c) => {
+app.post("/", authorizeAdminMiddleware, async (c) => {
   const { title, content } = await c.req.json();
   await createPost(title, content);
   return c.json({ message: "Post created successfully" }, 201);
 });
 
-app.delete("/:id", async (c) => {
+app.put("/:id", authorizeAdminMiddleware, async (c) => {
+  const id = c.req.param("id");
+  const { title, content, published } = await c.req.json();
+  const updatedPost = await updatePost(parseInt(id), title, content, published);
+  console.log(updatedPost);
+  return c.json({ message: "Post updated successfully" });
+});
+
+app.delete("/:id", authorizeAdminMiddleware, async (c) => {
+  const id = c.req.param("id");
+  await deletePost(parseInt(id));
   return c.json({ message: "User deleted successfully" });
 });
 
