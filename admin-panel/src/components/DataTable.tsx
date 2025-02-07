003da02331774c9b3bfc55f121ab8bc49908cbc2
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-02-07 17:54:13 +0100

    added a table component

---
+++ admin-panel/src/components/DataTable.tsx
@@ -0,0 +1,40 @@
+import { EllipsisVertical } from "lucide-react";
+
+interface Props {}
+
+function DataTable({}: Props) {
+  return (
+    <div className="overflow-x-auto">
+      <table className="table">
+        <thead>
+          <tr>
+            <th></th>
+            <th>Username</th>
+            <th>Email</th>
+            <th>Password</th>
+            <th>Role</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>1</th>
+            <td>
+              <div className="font-bold">Hart Hagerty</div>
+            </td>
+            <td>hart@gmail.com</td>
+            <td>*****</td>
+            <td className="font-bold">USER</td>
+            <th>
+              <button className="btn btn-ghost p-2 rounded-full">
+                <EllipsisVertical />
+              </button>
+            </th>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default DataTable;
